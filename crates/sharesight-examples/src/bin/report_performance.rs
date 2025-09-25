5b68abacc28ad01377e657f8b4d1ebab0b93f13f
Mark Catley <mark@catley.net.nz>
2025-09-26 11:59:53 +1200

    feat: add grouping to performance

--- crates/sharesight-examples/src/bin/report_performance.rs
+++ crates/sharesight-examples/src/bin/report_performance.rs
@@ -5,7 +5,8 @@ use clap::Parser;
 use sharesight_examples::init_logger;
 use sharesight_reqwest::Client;
 use sharesight_types::{
-    PerformanceShow, PerformanceShowParameters, PerformanceShowSuccess, DEFAULT_API_HOST,
+    GroupsList, GroupsListGroupsSuccess, GroupsListSuccess, PerformanceShow,
+    PerformanceShowParameters, PerformanceShowSuccess, DEFAULT_API_HOST,
 };
 
 /// Generate a 'performance' report using the sharesight API
@@ -20,6 +21,8 @@ struct Args {
     /// Lookback period in years
     #[clap(short, default_value = "5")]
     look_back_period_in_years: NonZeroU32,
+    #[clap(short)]
+    group: Option<String>,
     /// The access token to use the api.
     access_token: String,
 }
@@ -31,14 +34,27 @@ async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let client = Client::new_with_token_and_host(args.access_token, args.api_host);
     let portfolio_name = args.portfolio_name;
+    let group_name = args.group;
     let look_back_period_in_years = args.look_back_period_in_years.get() as i32;
 
-    let portfolios = client.build_portfolio_index().await?;
-
-    let portfolio = portfolios.find(&portfolio_name).unwrap_or_else(|| {
-        portfolios.log_error_for(&portfolio_name);
+    let portfolio_index = client.build_portfolio_index().await?;
+    let portfolio = portfolio_index.find(&portfolio_name).unwrap_or_else(|| {
+        portfolio_index.log_error_for(&portfolio_name);
         std::process::exit(0);
     });
+
+    let groups = client.execute::<GroupsList, GroupsListSuccess>(&()).await?;
+    let group = group_name.map(|group_name| {
+        groups
+            .find_group(&group_name)
+            .unwrap_or_else(|| std::process::exit(0))
+    });
+    let custom_group_id = group.and_then(|g| g.id.id());
+    let grouping = if custom_group_id.is_some() {
+        Some("custom_group".to_string())
+    } else {
+        group.and_then(|g| g.id.name())
+    };
 
     let today = Utc::now().date_naive();
     let inception_on = portfolio.inception_date;
@@ -47,7 +63,45 @@ async fn main() -> anyhow::Result<()> {
         .start_of_next_quarter()
         .add_years(-look_back_period_in_years);
 
-    println!("Start Date,End Date,Gain/Loss,Gain/Loss (%)");
+    let grouping_titles = if grouping.is_some() {
+        let performance_parameters = PerformanceShowParameters {
+            start_date: None,
+            end_date: None,
+            portfolio_id: portfolio.id,
+            consolidated: portfolio.consolidated,
+            include_sales: Some(true),
+            report_combined: None,
+            labels: None,
+            grouping: grouping.clone(),
+            custom_group_id,
+            include_limited: None,
+        };
+        let PerformanceShowSuccess {
+            report: performance_report,
+            ..
+        } = client
+            .execute::<PerformanceShow, _>(&performance_parameters)
+            .await?;
+
+        performance_report
+            .sub_totals
+            .into_iter()
+            .map(|s| s.group_name)
+            .collect::<Vec<_>>()
+    } else {
+        Vec::new()
+    };
+
+    print!("Start Date,End Date,Total,");
+    for title in &grouping_titles {
+        print!(",{},", title);
+    }
+    println!();
+    print!(",,Gain/Loss,Gain/Loss (%)");
+    for _title in &grouping_titles {
+        print!(",Gain/Loss,Gain/Loss (%)");
+    }
+    println!();
 
     while start_of_current_period >= inception_on {
         let performance_parameters = PerformanceShowParameters {
@@ -56,8 +110,8 @@ async fn main() -> anyhow::Result<()> {
             portfolio_id: portfolio.id,
             consolidated: portfolio.consolidated,
             include_sales: Some(true),
-            grouping: None,
-            custom_group_id: None,
+            grouping: grouping.clone(),
+            custom_group_id,
             include_limited: None,
             report_combined: None,
             labels: None,
@@ -69,13 +123,27 @@ async fn main() -> anyhow::Result<()> {
             .execute::<PerformanceShow, _>(&performance_parameters)
             .await?;
 
-        println!(
+        print!(
             "{},{},{},{}",
             performance_report.start_date,
             performance_report.end_date,
             performance_report.total_gain,
             performance_report.total_gain_percent
         );
+
+        for title in &grouping_titles {
+            let sub_total = performance_report
+                .sub_totals
+                .iter()
+                .find(|sub_total| &sub_total.group_name == title);
+            if let Some(sub_total) = sub_total {
+                print!(",{},{}", sub_total.total_gain, sub_total.total_gain_percent);
+            } else {
+                print!(",,");
+            }
+        }
+
+        println!();
 
         start_of_current_period = start_of_current_period.start_of_last_quarter();
         end_of_current_period = end_of_current_period.end_of_last_quarter();
@@ -125,5 +193,42 @@ impl DateExt for NaiveDate {
         let (year, month, day) = (self.year() + years, self.month(), self.day());
 
         NaiveDate::from_ymd_opt(year, month, day).unwrap()
+    }
+}
+
+trait GroupsListSuccessExt {
+    fn find_group<'a>(&'a self, group_name: &str) -> Option<&'a GroupsListGroupsSuccess>;
+}
+
+impl GroupsListSuccessExt for GroupsListSuccess {
+    fn find_group<'a>(&'a self, group_name: &str) -> Option<&'a GroupsListGroupsSuccess> {
+        let group = self.groups.iter().find(|p| p.name == group_name);
+
+        if let Some(group) = group {
+            Some(group)
+        } else {
+            eprint!("Unknown group: {}, ", group_name);
+
+            let mut names = self.groups.iter().map(|p| p.name.as_str());
+
+            match (names.next(), names.next_back()) {
+                (Some(name_start), Some(name_end)) => {
+                    eprint!("the group are: {}", name_start);
+                    for name in names {
+                        eprint!(", {}", name);
+                    }
+                    eprintln!(" or {}", name_end);
+                }
+                (Some(name), None) => {
+                    eprintln!("the only portfolio is: {}", name);
+                }
+                (None, None) => {
+                    eprintln!("there are no portfolios");
+                }
+                _ => unreachable!(),
+            }
+
+            None
+        }
     }
 }
